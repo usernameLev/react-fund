2994d789b9cc7f91ddf3e701a26a96dae598d56d
Lev <tyanlev1989@gmail.com>
2022-06-15 18:26:19 +0500

    check App

--- src/App.js
+++ src/App.js
@@ -1,82 +1,15 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-// import Counter from './components/Counter';
-// import ClassCounter from './components/ClassCounter';
+import React from 'react';
 import './styles/App.css';
-import PostItem from './components/PostItem';
-import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
-import PostForm from './components/PostForm';
-import MySelect from './components/UI/select/MySelect';
-import PostFilter from './components/PostFilter';
-import MyModal from './components/UI/MyModal/MyModal';
-import { usePosts } from './hooks/usePosts';
-import axios from 'axios';
-import PostService from './API/PostService';
-import Loader from './components/UI/Loader/Loader';
-import { useFetching } from './hooks/useFetching';
+import { BrowserRouter, Route } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'fd', body: 'mll' },
-    { id: 2, title: 'fgd', body: 'zs' },
-    { id: 3, title: 'bg', body: 'pl' },
-  ]);
-
-  const [filter, setFilter] = useState({ sort: '', query: '' });
-  const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
-  });
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  // Получаем post из дочернего компонента
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
   return (
-    <div className='App'>
-      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-
-      <hr style={{ margin: '15px 0' }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-
-      {postError && <h1>Произошла ошибка ${postError}</h1>}
-
-      {isPostsLoading ? (
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            marginTop: '50px',
-          }}
-        >
-          <Loader />
-        </div>
-      ) : (
-        <PostList
-          posts={sortedAndSearchedPosts}
-          title='Посты про JS'
-          remove={removePost}
-        />
-      )}
-    </div>
+    <BrowserRouter>
+      <Route path='/about'>
+        <About />
+      </Route>
+    </BrowserRouter>
   );
 }
 
